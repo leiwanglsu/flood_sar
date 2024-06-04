581462165695c2bb7cf3237b9809acfb476e3391
leiwanglsu <leiwang.lsu@gmail.com>
2024-06-03 21:37:15 -0500

    Add files via upload

--- geeData.py
+++ geeData.py
@@ -37,6 +37,8 @@ def main():
             s1_band = args['Sentinel-1 band']
             s1_date = args["Sentinel-1 date[yyyy-mm-dd,yyyy-mm--dd]"].split(",")
             crs = args['Coordinate system'] #e.g. 'EPSG:32119'
+            download_s1 = args["Download S1"]
+            download_jrc = args["Download jrc"]
     except Exception as e:
         print(e)    
         return
@@ -51,46 +53,64 @@ def main():
     if(url_jrc != ""):
         try:
             datasetJRC = ee.Image(url_jrc).select(jrc_band)
-            
-            task = ee.batch.Export.image.toDrive(
-                image= datasetJRC,
-                description= 'JCRwaterpixels',
-                region= bbox,
-                folder= 'AnyFolder',
-                scale= 30,
-                fileFormat= 'GeoTIFF',
-                maxPixels= 3784216672400,
-                crs= crs
-                )
-            task.start()
-            jrcWatcher = Process(target=Watcher,args=(task,))
-            jrcWatcher.start()
-            procs.append(jrcWatcher)
+            if download_jrc:
+                task = ee.batch.Export.image.toDrive(
+                    image= datasetJRC,
+                    description= 'JCRwaterpixels',
+                    region= bbox,
+                    folder= 'AnyFolder',
+                    scale= 30,
+                    fileFormat= 'GeoTIFF',
+                    maxPixels= 3784216672400,
+                    crs= crs
+                    )
+                task.start()
+                jrcWatcher = Process(target=Watcher,args=(task,))
+                jrcWatcher.start()
+                procs.append(jrcWatcher)
         except Exception as e:
             print(e)
     if(url_s1 != ""):
 
-        s1Img = ee.ImageCollection(url_s1).select([s1_band,"angle"]) \
+        s1ImgC = ee.ImageCollection(url_s1).select([s1_band,"angle"]) \
             .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')) \
             .filter(ee.Filter.eq('instrumentMode', 'IW')) \
             .filterBounds(bbox) \
-            .filterDate(s1_date[0],s1_date[1]).first()
-        s1ImgT = TerrainCorrection(s1Img)
-        s1ImgLee = refined_lee(s1ImgT.select(s1_band))
-        taskS1 = ee.batch.Export.image.toDrive(
-            image= s1ImgLee,
-            description= 'S1_Lee_filter',
-            region= bbox,
-            folder= 'AnyFolder',
-            scale= 10,
-            fileFormat= 'GeoTIFF',
-            maxPixels= 3784216672400,
-            crs= crs
-            )
-        taskS1.start()
-        s1Watcher = Process(target=Watcher,args=(taskS1,))
-        s1Watcher.start()
-        procs.append(s1Watcher)
+            .filterDate(s1_date[0],s1_date[1])
+        # Function to calculate intersection area.
+        
+        def calculate_intersection_area(image):
+            intersection = image.geometry().intersection(bbox, ee.ErrorMargin(1))
+            intersection_area = intersection.area()
+            return image.set('intersection_area', intersection_area)
+
+        # Map the function over the filtered collection.
+        with_area = s1ImgC.map(calculate_intersection_area)
+
+        # Sort the collection by the intersection area.
+        sorted_collection = with_area.sort('intersection_area', False)
+        s1Img = sorted_collection.first()
+        metadata = s1Img.getInfo()
+        with open(os.path.join(workspace,'s1_metadata.json'), 'w') as json_file:
+            json.dump(metadata, json_file, indent=4)
+            print(f"Sentinel-1 image metadata is written to s1_metadata.json")
+        if download_s1:
+            s1ImgT = TerrainCorrection(s1Img)
+            s1ImgLee = refined_lee(s1ImgT.select(s1_band))
+            taskS1 = ee.batch.Export.image.toDrive(
+                image= s1ImgLee.toFloat(),
+                description= 'S1_Lee_filter',
+                region= bbox,
+                folder= 'AnyFolder',
+                scale= 10,
+                fileFormat= 'GeoTIFF',
+                maxPixels= 3784216672400,
+                crs= crs
+                )
+            taskS1.start()
+            s1Watcher = Process(target=Watcher,args=(taskS1,))
+            s1Watcher.start()
+            procs.append(s1Watcher)
 
     for proc in procs:
         proc.join()
@@ -103,7 +123,7 @@ def Watcher(task):
     ee.Initialize()
      
     while task.active():
-        print(f'Polling for task (id: {task.id} with status of {task.status()['state']}')
+        print(f'Polling for task (id: {task.id} with status of {task.status()}')
         time.sleep(10)
     status = task.status()
     state = status['state']
